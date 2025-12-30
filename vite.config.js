eac509d21dbdfbef09f70364b7b85633baa59b13
1t1sCooL <mmalabugin@gmail.com>
2025-12-31 00:27:52 +0300

    checkout to deploy branch

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/DayOnEarth/", //this for my site. http://mmalabugin.ru/DayOnEarth/
+  base: "/DayOnEarth/",
 });
